94983b370265025cf24cbd2ed2cae2f006175c1e
EsmithAS <esmith_as@hotmail.com>
2021-01-30 19:36:43 -0500

    visor de imagenes integrado

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
 const selector = document.querySelector.bind(document);
+const visor = selector('#visor');
+
 selector("#btnMenu").addEventListener( 'click' , ()=> {
-    console.log('click');
     selector("#menu").classList.toggle('menu-x');
     selector("#btnMenu").classList.toggle('img-menu-x');
+})
+
+selector('#items-products').addEventListener('click',(e)=>{
+    const isVisualizar = e.target.parentElement.classList.contains('header');
+    if ( isVisualizar ) {
+        const srcImg = e.target.parentElement.querySelector('img').src;
+        const imgVisor = selector('#visor-img');
+        visor.classList.toggle('scale-0');
+        visor.classList.toggle('scale-100');
+        imgVisor.src = srcImg;
+    }
+})
+
+selector('#btnClose').addEventListener('click',()=>{
+    visor.classList.toggle('scale-0');
+    visor.classList.toggle('scale-100');
 })
