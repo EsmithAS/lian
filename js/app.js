7fe2e5aaa3b9ecf2b8f14e7151cf3474567a0437
EsmithAS <esmith_as@hotmail.com>
2021-02-02 19:18:57 -0500

    menu item focus resaltante agregado

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const selector = document.querySelector.bind(document);
 const visor = selector('#visor');
+const section = document.querySelectorAll('.section');
+let sections = {};
 
 selector("#btnMenu").addEventListener( 'click' , ()=> {
     selector("#menu").classList.toggle('menu-x');
@@ -21,3 +23,21 @@ selector('#btnClose').addEventListener('click',()=>{
     visor.classList.toggle('scale-0');
     visor.classList.toggle('scale-100');
 })
+
+section.forEach( e =>{
+    sections[e.id] = e.offsetTop;
+})
+
+window.onscroll = ()=>{
+    const scrollY = window.scrollY
+    for( let i in sections ){
+        if (sections[i] <= scrollY) {
+            const img = selector('#menu li img')
+            selector('.select').setAttribute('class','')
+            selector('a[href*='+ i +']').parentElement.setAttribute('class','select')
+            selector('a[href*='+ i +']').parentElement.appendChild(img)
+        }
+    }
+}
+
+
